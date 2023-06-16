fa81ce322880b8497118dabdccab8b1bada2be77
NumanTalukder <51156089+NumanTalukder@users.noreply.github.com>
2023-06-16 10:52:59 +0600

    Searchbar $ Custom Filter

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Navbar from './Navbar'
 import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Footer from './Footer'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+import SearchManufacturer from './SearchManufacturer'
 
-export { Navbar, Hero, CustomButton, Footer }
+export {
+  Navbar,
+  Hero,
+  CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  Footer,
+}

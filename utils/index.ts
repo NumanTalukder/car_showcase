69b39219cc75a01033589825a7dc3a0f7852d495
NumanTalukder <51156089+NumanTalukder@users.noreply.github.com>
2023-06-17 01:06:44 +0600

    car image fetch

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types'
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': '2aa1433bdfmshb19828fbc0ada26p1711d8jsn769ebfe660a3',
@@ -5,7 +7,7 @@ export async function fetchCars() {
   }
 
   const response = fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=s6',
     {
       headers: headers,
     }
@@ -28,4 +30,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
 
   return rentalRatePerDay.toFixed(0)
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  // bdnumantalukdercompany
+
+  const url = new URL('https://cdn.imagin.studio/getimage')
+
+  const { make, year, model } = car
+  url.searchParams.append('customer', 'bdnumantalukdercompany')
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  url.searchParams.append('angle', `${angle}`)
+
+  return `${url}`
 }
